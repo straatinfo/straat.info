5f96b43b1e161d55126e126bbd0ced3daed2fd69
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2019-04-16 05:37:11 +0800

    [Android](Login page)  - added logo  - added text

---
+++ app/src/main/java/com/straatinfo/straatinfo/Controllers/InitialActivity.kt
@@ -0,0 +1,50 @@
+package com.straatinfo.straatinfo.Controllers
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import com.straatinfo.straatinfo.R
+
+class InitialActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+
+        //Initializing the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        }
+    }
+
+    private fun hideSystemUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    private val mRunnable: Runnable = Runnable {
+        val intent = Intent(applicationContext, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
---
+++ app/src/main/java/com/straatinfo/straatinfo/Utilities/Constants.kt
@@ -0,0 +1,5 @@
+package com.straatinfo.straatinfo.Utilities
+
+const val BASE_URL = "https://straatinfo-backend-v2.herokuapp.com" // testing server
+
+// const val BASE_URL = "https://straatinfo-backend-v2-prod.herokuapp.com" // production server

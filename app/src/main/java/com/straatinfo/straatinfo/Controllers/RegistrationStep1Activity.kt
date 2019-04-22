5852aa4eeb20d6c4ff59a8110201a65680cc1b1f
ralfrotamola <ralf.rotamla4@gmail.com>
2019-04-22 16:34:17 +0800

    initial layout

---
+++ app/src/main/java/com/straatinfo/straatinfo/Controllers/RegistrationStep1Activity.kt
@@ -0,0 +1,13 @@
+package com.straatinfo.straatinfo.Controllers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.straatinfo.straatinfo.R
+
+class RegistrationStep1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_step1)
+    }
+}

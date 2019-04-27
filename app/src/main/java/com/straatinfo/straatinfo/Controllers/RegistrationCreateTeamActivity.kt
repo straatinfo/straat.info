3abeaf052871bed059e278189c9bef539ee47284
ralfrotamola <ralf.rotamla4@gmail.com>
2019-04-27 11:56:01 +0800

    added registration create team and polish some styles (step1, step2, step3)

---
+++ app/src/main/java/com/straatinfo/straatinfo/Controllers/RegistrationCreateTeamActivity.kt
@@ -0,0 +1,13 @@
+package com.straatinfo.straatinfo.Controllers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.straatinfo.straatinfo.R
+
+class RegistrationCreateTeamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_create_team)
+    }
+}

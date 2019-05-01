6589bdc37a7cd24629b03f0a3781b3ec3d8783d0
ralfrotamola <ralf.rotamla4@gmail.com>
2019-05-01 10:41:15 +0800

    added sidebar and reports activity

--- app/src/main/java/com/straatinfo/straatinfo/Controllers/MainActivity.kt
+++ app/src/main/java/com/straatinfo/straatinfo/Controllers/MainActivity.kt
@@ -1,13 +1,96 @@
 package com.straatinfo.straatinfo.Controllers
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.view.ViewCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.straatinfo.straatinfo.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        ViewCompat.setLayoutDirection(toolbar, ViewCompat.LAYOUT_DIRECTION_RTL)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        toolbar.setNavigationOnClickListener(navigationOnClickListener())
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.END)) {
+            drawer_layout.closeDrawer(GravityCompat.END)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_reports -> {
+                val intent = Intent(this, ReportsActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_home -> {
+                if (drawer_layout.isDrawerOpen(Gravity.END)) {
+                    drawer_layout.closeDrawer(Gravity.END)
+                } else {
+                    drawer_layout.isDrawerOpen(Gravity.END)
+                }
+            }
+            else -> {
+                if (drawer_layout.isDrawerOpen(Gravity.END)) {
+                    drawer_layout.closeDrawer(Gravity.END)
+                } else {
+                    drawer_layout.isDrawerOpen(Gravity.END)
+                }
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.END)
+        return true
+    }
+
+    private fun navigationOnClickListener() = View.OnClickListener {
+        if (drawer_layout.isDrawerOpen(Gravity.END)) {
+            drawer_layout.closeDrawer(Gravity.END)
+        } else {
+            drawer_layout.openDrawer(Gravity.END)
+        }
+        Toast.makeText(this, "toggle", Toast.LENGTH_LONG).show()
     }
 }

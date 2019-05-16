3b84ad74e450eb6a9c58676b1a5443f4a1ae64ec
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2019-05-17 06:23:23 +0800

    feedback

--- app/src/main/java/com/straatinfo/straatinfo/Controllers/LoginActivity.kt
+++ app/src/main/java/com/straatinfo/straatinfo/Controllers/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.straatinfo.straatinfo.Controllers
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.straatinfo.straatinfo.R
 import com.straatinfo.straatinfo.Services.AuthService
+import com.straatinfo.straatinfo.Services.RegexService
 import com.straatinfo.straatinfo.Services.UtilService
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_login.*
@@ -19,9 +24,13 @@ class LoginActivity : AppCompatActivity() {
 
         App.prefs.isLoggedIn = false
 
+        loadFormSettings()
+
 //        loginBtn.setOnClickListener(View.OnClickListener {
 //            Toast.makeText(this, "hello", Toast.LENGTH_LONG).show()
 //        })
+
+
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -60,6 +69,37 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
             .run {  }
+    }
+
+    private fun loadFormSettings () {
+        val loginButton = findViewById<Button>(R.id.loginBtn)
+        val emailInput = findViewById<EditText>(R.id.loginEmailET)
+        val passwordInput = findViewById<EditText>(R.id.loginPasswordET)
+        loginButton.isEnabled = false
+
+        val settings = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                loginButton.isEnabled = RegexService.testEmail(emailInput.text.toString()) && emailInput.text.toString() != "" && passwordInput.text.toString() != ""
+            }
+        }
+
+        emailInput.addTextChangedListener(settings)
+
+        emailInput.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus && !RegexService.testEmail(emailInput.text.toString())) {
+                val dialog = UtilService.showDefaultAlert(this, getString(R.string.email_address), getString(R.string.error_invalid_email))
+                dialog.show()
+            }
+        }
+
+        passwordInput.addTextChangedListener(settings)
+
     }
 
     fun onRegisterClick (view: View) {
